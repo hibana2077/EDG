8637cc176bca9e1c36142f099dc150f88c491e92
hibana2077 <hibana2077@gmail.com>
2025-07-02 14:32:38 +0800

    fix: Simplify RandomSharpness initialization by removing unused parameter

--- src/network/augnetv3.py
+++ src/network/augnetv3.py
@@ -17,8 +17,7 @@ class AugNet(nn.Module):
 
         # ＝ 光度增強 ＝
         self.st_1 = nn.Parameter(torch.tensor(0.5))
-        self.st_2 = nn.Parameter(torch.tensor(2.))
-        self.rs = K.RandomSharpness(saturation = (self.st_1,self.st_2),p=1.)
+        self.rs = K.RandomSharpness(self.st_1,p=1.)
         
         # 按順序收好，方便迴圈處理
         self.aug_layers = nn.ModuleList([
