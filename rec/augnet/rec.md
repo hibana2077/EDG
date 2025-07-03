30ab368bac71db3c3733f08844308f161a546dd9
hibana2077 <hibana2077@gmail.com>
2025-07-04 00:10:47 +0800

    feat: Implement code changes to enhance functionality and improve performance

--- rec/augnet/rec.md
+++ rec/augnet/rec.md
@@ -1,5 +1,21 @@
 # Augnet - experimental record
 
-| Augnet architecture | Highest accuracy| w/o Augmentation | Notes |
-|---------------------|------------------|------------------|-------|
-| Kornia Normalization | 0.4961           | 0.4625           |    baseline model: tiny_vit_21m_224   |
+| Augnet architecture | Highest accuracy| w/o Augmentation | Augnet Loss | Notes |
+|---------------------|------------------|------------------|-------------|-------|
+| Kornia Normalization | 0.4961           | 0.4625           |   0    | baseline model: tiny_vit_21m_224 |
+| Kornia Norm + Rand Sharp | 0.4102           | 0.4625           |  0.7762     | baseline model: tiny_vit_21m_224 |
+| Kornia Norm + Rand Const | 0.3750           | 0.4625           |  0.0373     | baseline model: tiny_vit_21m_224 |
+
+## image
+
+### Kornia Normalization
+
+![Kornia Normalization](./norm.png)
+
+### Kornia Norm + Rand Sharp
+
+![Kornia Norm + Rand Sharp](./norm_rs.png)
+
+### Kornia Norm + Rand Const
+
+![Kornia Norm + Rand Const](./norm_rc.png)
--- src/network/augnetv3.py
+++ src/network/augnetv3.py
@@ -5,6 +5,20 @@ import kornia.augmentation as K
 from timm.models.tiny_vit import TinyVitBlock
 from timm.models.convnext import ConvNeXtBlock
 
+class LearnableContrast(nn.Module):
+    def __init__(self, init_factor: float = 0.5):
+        super().__init__()
+        # 單一 learnable factor，或你想學兩端範圍都可以
+        self.factor = nn.Parameter(torch.tensor(init_factor))
+
+    def __repr__(self):
+        return f"factor: {self.factor}"
+    
+    def forward(self, x):
+        # (B,C,H,W)；center = 每張圖各自的平均亮度
+        mean = x.mean(dim=[2,3], keepdim=True)
+        return torch.clamp((x - mean) * self.factor + mean, 0., 1.)
+
 class AugNet(nn.Module):
     def __init__(self, dim: int = 224, num_heads:int=8):
         super().__init__()
@@ -16,13 +30,12 @@ class AugNet(nn.Module):
         # ＝ 幾何增強 ＝
 
         # ＝ 光度增強 ＝
-        self.st_1 = nn.Parameter(torch.tensor(0.5))
-        self.rs = K.RandomSharpness(self.st_1,p=1.)
+        self.rc = LearnableContrast()
         
         # 按順序收好，方便迴圈處理
         self.aug_layers = nn.ModuleList([
-            self.norm,
-            self.rs,
+            # self.norm,
+            self.rc,
         ])
 
     def show(self):
